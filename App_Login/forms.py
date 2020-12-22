cc11746a92e786ef654f61906b124b934ebd173c
neel rai <2019ugee056@gmail.com>
2020-12-22 22:12:02 +0530

    login profile change-profile-pic

--- App_Login/forms.py
+++ App_Login/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import UserProfile
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(label = 'Email Address', required = True)
     class Meta:
         model=User
         fields = ('username','email', 'password1', 'password2')
+
+class ProfilePic(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['profile_pic']
